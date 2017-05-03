9719ae604aac2427e9eda3a9a6cfa08d5f9adc9e
Tariq Islam <tislam@redhat.com>
2017-05-03 11:31:48 -0400

    'cleaning up'

--- pipeline-setup.sh
+++ pipeline-setup.sh
@@ -19,12 +19,12 @@ if [ -z "$username" ]; then
 	username="openshift-dev"
 fi
 
-echo "Enter the url for your repository. If it's a specific branch you'd like to use, please append the branch to the url with '#<branch_name>' [default: https://github.com/tariq-islam/bluegreen#ocp33-pipeline]: "
+echo "Enter the url for your repository. If it's a specific branch you'd like to use, please append the branch to the url with '#<branch_name>' [default: https://github.com/tariq-islam/bluegreen]: "
 
 read repository_path
 
 if [ -z "$repository_path" ]; then
-	repository_path="https://github.com/tariq-islam/bluegreen#ocp33-pipeline"
+	repository_path="https://github.com/tariq-islam/bluegreen"
 	echo "Using default repository at : " $repository_path
 fi
 
@@ -33,6 +33,7 @@ oc new-project app-dev --display-name="Application Development Environment"
 oc new-app --name=bluegreen "$repository_path"
 oc expose service bluegreen
 oc new-app "$jenkins_image" -p JENKINS_PASSWORD=password
-oc create -f <path to your build config here>
+oc delete bc bluegreen
+oc create -f https://raw.githubusercontent.com/tariq-islam/bluegreen/bluegreen-bc.yml
 oc new-project app-qa --display-name="Application QA Environment"
 oc new-project app-prod --display-name="Application Production Environment"
